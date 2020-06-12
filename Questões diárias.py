8e6c21189845dee68152a14969844bea82909433
RLBrMd <66687987+RLBrMd@users.noreply.github.com>
2020-06-12 18:34:23 -0300

    Add files via upload

---
+++ Questões diárias.py
@@ -0,0 +1,15 @@
+print ('Enem:')
+from random import randint
+print(randint(2017,2019))
+print ('Linguagens:')
+from random import randint
+print(randint(1,45))
+print ('Humanas:')
+from random import randint
+print(randint(46,90))
+print ('Natureza:')
+from random import randint
+print(randint(91,135))
+print ('Matemática:')
+from random import randint
+print(randint(136,180))
